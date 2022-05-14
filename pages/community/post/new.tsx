6c7f928cd6791f4e4b508e3dbdcc79e53c71b874
ista1024 <ista1024@gmail.com>
2022-05-14 17:15:53 +0900

    commit

--- pages/community/post/new.tsx
+++ pages/community/post/new.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
 
   ◎ 사진 및 외부 콘텐츠 첨부시 영향력 상승!
   ◎ 뉴스, 블로그 등 외부 콘텐츠는 https:// 링크를 붙여 넣으세요. 본문에 썸네일로 표시됩니다.
-  ◎ 광고글 금지. 서비스 이용이 제한됩니다.`;
+  ◎ 광고글 금지. 서비스 이용이 제한됩니다. `;
   return (
     <div className={styles.container}>
       <Head>
