21e0740e166cb8c54efa15a5d6c72516ea14955b
ista1024 <ista1024@gmail.com>
2022-05-14 23:18:04 +0900

    post new

--- pages/community/post/new.tsx
+++ pages/community/post/new.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
@@ -6,6 +6,25 @@ import Image from "next/image";
 import styles from "../../../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const [image, setImage] = useState(null);
+  const [createObjectURL, setCreateObjectURL] = useState(null);
+
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    if (inputEl.current) {
+      inputEl.current!.click();
+    }
+  };
+
+  const uploadToClient = (event: any) => {
+    if (event.target.files && event.target.files[0]) {
+      const i = event.target.files[0];
+
+      setImage(i);
+      setCreateObjectURL(URL.createObjectURL(i));
+    }
+  };
   const contentPlaceHolder: string = `내용을 작성해주세요.\n
 
   ◎ 사진 및 외부 콘텐츠 첨부시 영향력 상승!
@@ -30,7 +49,18 @@ const Home: NextPage = () => {
         <div>
           <textarea placeholder={contentPlaceHolder} />
         </div>
-        <button>사진</button>
+        {createObjectURL ? (
+          <Image src={createObjectURL} width={160} height={160} alt="이미지" />
+        ) : null}
+        <input
+          type="file"
+          accept="image/*"
+          style={{ display: "none" }}
+          ref={inputEl}
+        />
+        <label htmlFor="contained-button-file">
+          <button onClick={handleClick}>사진</button>
+        </label>
       </main>
     </div>
   );
