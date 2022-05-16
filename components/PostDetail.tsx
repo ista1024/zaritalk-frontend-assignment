c03acef22d00be2ad2e2b2afaf2d747f8e36af43
ista1024 <ista1024@gmail.com>
2022-05-16 22:31:58 +0900

    viewCount와 좋아요 api통신, 댓글 코드 예시 작성

--- components/PostDetail.tsx
+++ components/PostDetail.tsx
@@ -1,34 +1,57 @@
 import type { NextPage } from "next";
-import Link from "next/Link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 
 import { postData } from "../types/communityType";
 
-// MdVisibility: 눈알, MdThumbUp: 좋아요 색, MdThumbUpOffAlt: 좋아요 테두리
-import { MdVisibility, MdThumbUp, MdThumbUpOffAlt } from "react-icons/md";
+// MdThumbUp: 좋아요 색, MdThumbUpOffAlt: 좋아요 테두리
+import { MdThumbUp, MdThumbUpOffAlt } from "react-icons/md";
 // BsChatDotsFill: 댓글 색, BsChatDots: 댓글 테두리
 import { BsChatDotsFill, BsChatDots } from "react-icons/bs";
 import { compareDate } from "../utils/util";
-
-import styles from "../styles/global.module.css";
 
 interface postProps {
   data: postData;
 }
 
 const PostDetail: NextPage<postProps> = ({ data }: postProps) => {
-  const [isButtonClicked, setIsButtonClicked] = useState<number>(0);
+  // viewCount 반영을 위한 api 통신
+  /* 코드예제
+   * const result = await axios.post(
+   *   `/api/community/post/viewCount/${post.pk}`,
+   *   {
+   *     postPk: post.pk,
+   *     viewer: viewer | null,
+   *   }
+   * );
+   */
+
   const [post, setPost] = useState<postData>(data);
 
   // 좋아요 눌렀는지 확인 및 카운트 변경
   const [likes, setLikes] = useState<boolean>(false);
   const [likesCount, setLlkesCount] = useState<number>(data.likeCount);
+  // 댓글 표시 여부
+  const [isCommentShow, setIsCommentShow] = useState<boolean>(false);
 
   // 좋아요 핸들링
   const handleLikes = () => {
     setLikes(!likes);
     likes ? setLlkesCount(likesCount - 1) : setLlkesCount(likesCount + 1);
+    // 실제로 api를 통해 좋아요 여부를 서버와 통신해야 함
+    /* 코드예시
+     * const result = await axios.post(
+     *   `/api/community/post/like/${post.pk}`,
+     *   {
+     *     postPk: post.pk,
+     *     isLike: !likes
+     *   }
+     * );
+     * if (result.data.isSuccess) {
+     *   setLikes(!likes);
+     *   likes ? setLlkesCount(likesCount - 1) : setLlkesCount(likesCount + 1);
+     * }
+     */
   };
 
   // 이미지 src 파서
@@ -41,7 +64,7 @@ const PostDetail: NextPage<postProps> = ({ data }: postProps) => {
     const regex = /(https?:\/\/[^\s]+)/g;
     const match = text.match(regex);
     if (match) {
-      const link = `<a className="underline decoration-sky-500" href=${match[0]}>${match[0]}</a>`;
+      const link = `<a class="underline decoration-sky-500" href=${match[0]}>${match[0]}</a>`;
       return text.replace(match[0], link);
     } else {
       return text;
@@ -53,8 +76,7 @@ const PostDetail: NextPage<postProps> = ({ data }: postProps) => {
   })();
 
   return (
-    <div className={"flex flex-col  mb-1 py-4 bg-white "}>
-      {/* <div>{post.pk}</div> */}
+    <div className={"flex flex-col  w-full mb-1 py-4 bg-white "}>
       <div>
         <div className="flex align-center my-2">
           <div className="flex self-center mx-2">
@@ -83,17 +105,22 @@ const PostDetail: NextPage<postProps> = ({ data }: postProps) => {
         </div>
       </div>
       {!!post.imageUrl ? (
-        <div className="my-2 w-full relative">
+        <div className="my-2 w-full h-full relative">
           <Image
             src={imgSrc(post.imageUrl)}
-            layout="fill"
+            width="100%"
+            height="100%"
+            layout="responsive"
             alt="post"
-            objectFit="cover"
+            objectFit="contain"
           />
         </div>
       ) : null}
       <div className="flex text-gray-500  align-center">
-        <div className="flex mx-2" onClick={() => handleLikes()}>
+        <div
+          className="flex bg-gray-100 mx-2 p-1 pl-2 rounded-lg"
+          onClick={() => handleLikes()}
+        >
           {likes ? (
             <MdThumbUp className="self-center" />
           ) : (
@@ -101,10 +128,34 @@ const PostDetail: NextPage<postProps> = ({ data }: postProps) => {
           )}
           <span className="mx-2">{likesCount}</span>
         </div>
-        <div className="flex mx-2">
-          <BsChatDots className="self-center" />
+        <div
+          className="flex bg-gray-100 mx-2  p-1 pl-2 rounded-lg"
+          onClick={() => setIsCommentShow((isCommentShow) => !isCommentShow)}
+        >
+          {isCommentShow ? (
+            <BsChatDotsFill className="self-center" />
+          ) : (
+            <BsChatDots className="self-center" />
+          )}
           <span className="mx-2">{post.commentCount}</span>
         </div>
+      </div>
+      <div>
+        {isCommentShow
+          ? post.commentCount > 0
+            ? [...Array(post.commentCount)].map((_, index) => (
+                <div
+                  key={`comment ${index}`}
+                  className="flex justify-between my-2"
+                >
+                  <div className="flex self-center mx-2">
+                    <span className="text-gray-500">댓글작성자_{index}</span>
+                    <span className="text-gray-500 mx-2">댓글 {index}</span>
+                  </div>
+                </div>
+              ))
+            : null
+          : null}
       </div>
     </div>
   );
